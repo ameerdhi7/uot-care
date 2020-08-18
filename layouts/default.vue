86454d16ad955cb1d1bc66ec2d54642ee7230288
Ameer Dhiaa <dhiaaback@gmail.com>
2020-08-18 17:20:23 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Nuxt/>
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Baloo+Tamma+2:wght@400;600&display=swap');
+
+  html {
+    font-family: 'Baloo Tamma 2', cursive;
+  }
+</style>
---
+++ plugins/lottie-vue.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import Lottie from 'lottie-vuejs'
+Vue.use(Lottie);
